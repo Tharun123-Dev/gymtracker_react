f6e613dfe3b09da4bec1fbd44c8fd5d14695355b
Tharun123-Dev <anjali.tharun9949@gmail.com>
2026-02-04 16:12:16 +0530

    vercel

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -60,12 +60,14 @@ CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = False
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    # "http://localhost:5173",
+    "https://gymtracker-react.vercel.app/",
     
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
+    # "http://localhost:5173",
+    "https://gymtracker-react.vercel.app/",
 ]
 
 SECRET_KEY = os.getenv("SECRET_KEY")
