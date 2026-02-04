1a84376568f58b36ae10e7a050c76775c511becd
Tharun123-Dev <anjali.tharun9949@gmail.com>
2026-02-04 16:30:01 +0530

    axios

--- frontend/frontend/src/Api/Axios.js
+++ frontend/frontend/src/Api/Axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://127.0.0.1:8000",
   baseURL: "https://gymtracker-react.onrender.com",
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default api;
